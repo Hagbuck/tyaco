30796aff6a09904069d61e86930f58e1459b46f1
Hagbuck <anthony.vuillemin@outlook.fr>
2020-03-11 16:31:09 +0100

    Add comment route API

--- back/api/index.js
+++ back/api/index.js
@@ -3,10 +3,16 @@ const router = express.Router();
 
 const user = require('./routes/user');
 const contest = require('./routes/contest');
+const constraint = require('./routes/constraint');
+const submission = require('./routes/submission');
+const comment = require('./routes/comment');
 
 module.exports = () => {
     router.use('/user',user());
     router.use('/contest',contest());
+    router.use('/constraint',constraint());
+    router.use('/submission',submission());
+    router.use('/comment',comment());
 
     return router;
 };
---
+++ back/api/routes/comment.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+module.exports = () => {
+
+	/** 
+	 * Edit a specific comment
+	 */
+	router.put('/:comment_id', (req, res) => {
+
+	});
+
+	/** 
+	 * Delete a specific comment
+	 */
+	router.delete('/:comment_id', (req, res) => {
+
+	});
+
+	return router;
+};
